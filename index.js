0394e79b0c6b85df74bce438a145053cd78f6514
Artiom <ArtemTaturevich@yandex.ru>
2024-06-30 23:04:53 +0300

    oes

--- index.js
+++ index.js
@@ -1,12 +1,120 @@
-let buttonOpen = document.querySelector(".new")
-let modal = document.querySelector(".modal")
+const swiper = new Swiper('.mySwiper', {
+  loop: true,
 
-buttonOpen.addEventListener('click', function () {
-    modal.classList.add("modal__show")
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    slidesPerView: "1.15"
+  });
+
+var fotos = [
+  './fotos/Bitmaplenovo.svg',
+  './fotos/Bitmapsamsung.svg',
+  './fotos/Bitmapapple.svg',
+  './fotos/Bitmapview.svg',
+  './fotos/Bitmapbosh.svg',
+  './fotos/Bitmaphp.svg',
+  './fotos/Bitmapapple.svg',
+  './fotos/Bitmaphp.svg',
+  './fotos/Bitmapacer.svg',
+  './fotos/Bitmapsony.svg'
+]
+
+var list = document.querySelector('.main__ul');
+var items = list.children;
+var btnUp = document.querySelector('.pocaz')
+var btnPrev = document.querySelector('.scrit')
+var taskTemplate = document.querySelector('#main__li').content;
+var newItemTempalte = taskTemplate.querySelector('.main__li')
+
+
+var def = 6;
+
+if (window.innerWidth < 1120) {
+  def = 6
+} else {
+  def = 8
+}
+
+for (var i = 0; i < def; i++) {
+  var form = newItemTempalte.cloneNode(true)
+  var image = form.querySelector('.main__logo')
+  image.src = fotos[i]
+  
+  list.appendChild(form)
+}
+
+
+btnUp.addEventListener('click', () => {
+  def = fotos.length;
+
+  for (var i = 6; i < def; i++) {
+    var form = newItemTempalte.cloneNode(true)
+    var image = form.querySelector('.main__logo')
+    image.src = fotos[i]
+    
+    list.appendChild(form)
+
+    deleteEl(items[i])
+  }
+
+    btnUp.classList.add('none')
+    btnPrev.style.display = 'block'
 })
 
-let buttonClose = document.querySelector(".modal__close") 
+function deleteEl(item) {
+  btnPrev.addEventListener('click', () => {
+      item.remove()
+      btnUp.classList.remove('none');
+      btnPrev.style.display = 'none';
+  })
+}
 
-buttonClose.addEventListener('click', function () {
-    modal.classList.remove("modal__show")
+
+
+/*
+var def = 6;
+
+for (var i = 0; i < def; i++) {
+  var form = newItemTempalte.cloneNode(true)
+  var image = form.querySelector('.main__logo')
+  image.src = fotos[i]
+  
+  list.appendChild(form)
+}
+
+
+btnUp.addEventListener('click', (foto) => {
+  def = fotos.length;
+
+  for (var i = 6; i < def; i++) {
+    var form = newItemTempalte.cloneNode(true)
+    var image = form.querySelector('.main__logo')
+    image.src = fotos[i]
+    
+    list.appendChild(form)
+  }
+
+    btnUp.classList.add('none')
+    btnPrev.style.display = 'block'
 })
+
+
+
+for (var i = 6; i < fotos.length; i++) {
+  deleteEl(items[i])
+}
+
+function deleteEl(item) {
+  btnPrev.addEventListener('click', () => {
+      console.log(item)
+      btnUp.classList.remove('none');
+      btnPrev.style.display = 'none';
+  })
+}
+
+
+
+*/
